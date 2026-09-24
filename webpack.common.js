0aeed648b29eea0d5cf4df013b371cc600e9bf14
Jesús Martín Rosas Marín <72567940+MarkTwin25@users.noreply.github.com>
2026-09-23 22:17:01 -0600

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+import { resolve } from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: resolve(import.meta.dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+    ],
+  },
+};
